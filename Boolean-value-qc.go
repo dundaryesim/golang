28756c60bdea82dd898879e19bce922a95607fc2
dundaryesim <kuranyesim@gmail.com>
2021-03-28 20:31:00 +0200

    Delete empty file

--- Boolean-value-qc.go
+++
@@ -1,3 +0,0 @@
-//1 Emerging from the cave, your eyes meet the blinding midday sun. How do you declare a Boolean variable named wearShades?
-//2 There is a sign near the cave entrance. How can you determine if the command contains the word “read”?
-package main
