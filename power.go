1b162dc132415026d7003b7d49e154595fde3142
M. Serhat Dundar <msdundars@gmail.com>
2021-05-06 00:44:02 +0200

    fix power example

--- power.go
+++ power.go
@@ -2,24 +2,24 @@ package main
 
 import "fmt"
 
-func pow(sayı, üs int) int {
-	if üs == 0 {
-		return 1
-	}
-	if üs < 0 {
+func pow(rakam, us int) int {
+	if us < 0 {
 		return 0
 	}
 
-	total := 1
-	for i := 0; i < üs; i++ {
-		total = total * sayı
-
+	if us == 0 {
+		return 1
 	}
+
+	total := 1
+
+	for i := 0; i < us; i++ {
+		total = total * rakam
+	}
+
 	return total
 }
 
 func main() {
-
-	fmt.Println(pow(2, -1))
-
+	fmt.Println(pow(3, 4))
 }
